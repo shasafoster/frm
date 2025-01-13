59e32fbcbe0a679dc16c114e6cb347bb5ba79b51
Shasa Foster <shasa.foster@gmail.com>
2025-01-13 21:54:37 +1300

    Add imports to init file for cleaner imports in test and example files

--- frm/pricing_engine/__init__.py
+++ frm/pricing_engine/__init__.py
@@ -1,3 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from frm.pricing_engine.black76_bachelier import * # noqa F401
+from frm.pricing_engine.clewlow_strickland_1_factor import * # noqa F401
+from frm.pricing_engine.cosine_method_generic import * # noqa F401
+from frm.pricing_engine.garman_kohlhagen import * # noqa F401
+from frm.pricing_engine.geometric_brownian_motion import * # noqa F401
+from frm.pricing_engine.heston import * # noqa F401
+from frm.pricing_engine.hw1f import * # noqa F401
+from frm.pricing_engine.monte_carlo_generic import * # noqa F401
+from frm.pricing_engine.sabr import * # noqa F401
+from frm.pricing_engine.volatility_generic import * # noqa F401
+
 
